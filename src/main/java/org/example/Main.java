e1fde064aaffc21291f20fa01880be9cad6f7fce
Valeriia Sosiedka <106317830+ValSos@users.noreply.github.com>
2023-07-19 22:58:19 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         SumCalculator sumcalc = new SumCalculator();
-        SumCalculator sumcalc2 = new SumCalculator();
     }
 }
